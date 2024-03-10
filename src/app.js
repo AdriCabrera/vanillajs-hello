430f68d54b93a817d499e2f404558bddc7ac5be2
AdriCabrera <160674673+AdriCabrera@users.noreply.github.com>
2024-03-10 16:32:18 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,30 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
 };
+function generateExcuse() {
+  let who = ["My dog", "The cat"];
+  let action = [" eat my", " threw my", " stole my", " bite my"];
+  let possetion = [" food", " car", " shoe"];
+  let where = [" on the street.", " in my house.", " in the toilet."];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let possetionIndex = Math.floor(Math.random() * possetion.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    who[whoIndex] +
+    "" +
+    action[actionIndex] +
+    "" +
+    possetion[possetionIndex] +
+    "" +
+    where[whereIndex]
+  );
+}
